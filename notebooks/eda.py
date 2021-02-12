ea4cb05e5f2c63adb2fdbb3279009247cc8d458c
pierre-si <62605527+pierre-si@users.noreply.github.com>
2021-02-12 22:06:13 +0100

    eda

--- notebooks/eda.py
+++ notebooks/eda.py
@@ -3,16 +3,6 @@ import pandas as pd
 #%%
 df = pd.read_csv('../data/datasets/contents.csv', dtype={'Category':'category'})
 df
-#%%
-summary_category_data = []
-    for cat in categories:
-        files_count = len(os.listdir(os.path.join(path, cat)))
-        summary_category_data.append([cat, files_count])
-
-    summary_category = pd.DataFrame(columns=['Category', 'Number_of_files'], data=summary_category_data)
-    summary_category
-#%%
-df.Category.cat.codes
 # %%
 df.groupby('Category').count()[['Content']].plot.bar()
 df['News length'] = df['Content'].str.len()
